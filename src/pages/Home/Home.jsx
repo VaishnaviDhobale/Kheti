300c35a3f70432c50b63260c18852729ac12d168
VaishnaviDhobale <masaieducation17@gmail.com>
2024-10-01 15:36:20 +0530

    trying to build

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,39 @@
+import axios from "axios";
+import { useState } from "react";
+
 export const Home = () => {
+  const [input,setInput] = useState("");
+  const handleInput = (event) => {
+    setInput({name:event.target.value});
+  }
+
+  const handleSubmit = async() => {
+    try{
+      console.log(input)
+      const response = await axios.post("http://localhost:8080/admins/addAdmin",input);
+      console.log(response.data.success);
+    }catch(error){
+      console.log(error);
+    }
+  }
   return (
     <>
-        <div className="flex justify-center items-center gap-20">
-          <div className="flex flex-col justify-center items-center">
-            <h1>SEEDS</h1>
-            <h2>2303</h2>
-          </div>
-
-          <div className="flex flex-col justify-center items-center">
-            <h1>MATERIALS</h1>
-            <h2>2303</h2>
-          </div>
-
-          <div className="flex flex-col justify-center items-center">
-            <h1>AUSADS</h1>
-            <h2>2303</h2>
-          </div>
-        </div>
+      <div className="flex justify-center items-center gap-20 mt-20">
+        <form>
+          <input
+            className="border p-2 outline-none rounded-lg"
+            type="text"
+            placeholder="Enter name"
+            onChange={handleInput}
+          />
+          <input
+            className="bg-emerald-700 p-2 ml-5 text-white font-bold rounded-md cursor-pointer"
+            type="button"
+            value="Add Name"
+            onClick={handleSubmit}
+          />
+        </form>
+      </div>
     </>
   );
 };
